a725d9c0f3671c3eca8a7a2ebdfc1152e4e7f85b
CADET <maxi97115@gmail.com>
2019-12-22 19:33:21 +0100

    Création de la plage d'accueil pour choisir sa compétition

--- app/src/main/java/com/example/androidproject/FootRestApi.java
+++ app/src/main/java/com/example/androidproject/FootRestApi.java
@@ -9,11 +9,12 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
     public interface FootRestApi {
         @Headers("X-Auth-Token:d8a030d395484245910e03565ec4bb64")
-        @GET("competitions/FL1/teams")
-        Call<RestFootResponse> getFootList();
+        @GET("competitions/{id}/teams")
+        Call<RestFootResponse> getFootList(@Path("id") String id);
     }
 
--- app/src/main/java/com/example/androidproject/view/MainActivity.java
+++ app/src/main/java/com/example/androidproject/view/MainActivity.java
@@ -8,29 +8,34 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.androidproject.Injection;
 import com.example.androidproject.R;
+import com.example.androidproject.controller.LeagueController;
 import com.example.androidproject.controller.MainController;
 import com.example.androidproject.controller.SplashController;
 import com.example.androidproject.modele.Foot;
 
 import java.util.List;
 
-public class MainActivity extends Activity implements MyAdapter.OnFootListener {
+public class MainActivity extends Activity implements MyAdapter.OnFootListener, View.OnClickListener {
 
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private LeagueController leagueFragment;
     private SplashController splashFragment;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         splashFragment = (SplashController) getFragmentManager().findFragmentById(R.id.load_fragment);
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -39,9 +44,25 @@ public class MainActivity extends Activity implements MyAdapter.OnFootListener {
                 splashFragment.getView().setVisibility(View.INVISIBLE);
             }
         }, 9000);
+        leagueFragment = (LeagueController)getFragmentManager().findFragmentById(R.id.league_fragment);
+
+        Button bl1 = findViewById(R.id.BL1);
+        bl1.setOnClickListener(this);
+        Button fl1 = findViewById(R.id.FL1);
+        fl1.setOnClickListener(this);
+        Button cl = findViewById(R.id.CL);
+        cl.setOnClickListener(this);
+        Button sa = findViewById(R.id.SA);
+        sa.setOnClickListener(this);
+        Button pl = findViewById(R.id.PL);
+        pl.setOnClickListener(this);
+        Button pd = findViewById(R.id.PD);
+        pd.setOnClickListener(this);
+
+
         SharedPreferences sharedPreferences = this.getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
         MainController controller = new MainController(this, Injection.getRestApiInstance(), sharedPreferences);
-        controller.start();
+        controller.start("FL1");
 
     }
 
@@ -60,5 +81,42 @@ public class MainActivity extends Activity implements MyAdapter.OnFootListener {
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra("Id", id);
         startActivity(intent);
+    }
+
+    public void onLeagueClick(String id){
+        leagueFragment.getView().setVisibility((View.INVISIBLE));
+        SharedPreferences sharedPreferences = this.getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
+        MainController controller = new MainController(this, Injection.getRestApiInstance(), sharedPreferences);
+        controller.start(id);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case  R.id.BL1 :
+                Log.d("test btn", "ça marche !");
+                onLeagueClick("BL1");
+                break;
+
+            case R.id.FL1 :
+                onLeagueClick("FL1");
+                break;
+
+            case R.id.CL :
+                onLeagueClick("CL");
+                break;
+
+            case R.id.PD :
+                onLeagueClick("PD");
+                break;
+
+            case R.id.PL :
+                onLeagueClick("PL");
+                break;
+
+            case R.id.SA :
+                onLeagueClick("SA");
+                break;
+        }
     }
 }
